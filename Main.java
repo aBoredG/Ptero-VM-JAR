a80e1aa7bb5590fc4d2b388a26bd48c396066562
Triassic <40073455+RealTriassic@users.noreply.github.com>
2021-11-14 14:03:35 +0200

    Add notice when Installer.sh file is missing
    This is to warn users that are using hosts that potentially blacklist the links used to download Installer.sh.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 class Main {
     public static void main(String[] args) {
         try {
@@ -24,6 +26,25 @@ class Main {
             System.out.print("\033[H\033[2J");
             System.out.println("Downloading Installer.sh from Ptero-VM's GitHub...");
             curl.start().waitFor();
+
+            // Check if Installer.sh exists and if it does not, notify the user.
+            File installerFile = new File("installer.sh");
+            boolean installerExists = installerFile.exists();
+
+            if (installerExists) {
+                // do nothing and just continue.
+            } else {
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println((char) 27 + "[31m" + "An error has occured whilst trying to download Installer.sh.");
+                    System.out.println("\n\n");
+                    System.out.println("The hosting service you are currently using has blocked the Installer.sh download link.");
+                    System.out.println();
+                    System.out.println("Please download the Installer.sh file manually from the link below and upload it to the server to bypass this.");
+                    System.out.println("https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh");
+                    System.out.println("\n\n\n\n\n\n\n\n");
+                    System.exit(0);
+             }
 
             // Starts the installer
             String[] installer_cmd = {"bash", "installer.sh"};
