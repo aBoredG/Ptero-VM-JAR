a95cff4ee54c29658d581753abc7e22dca0b645f
aBoredG <95982400+aBoredG@users.noreply.github.com>
2021-12-11 22:21:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,8 @@
 import java.io.File;
 
 class Main {
-    public static void main(String[] args) {
-        try {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-
-            System.out.println("\n\n\n\n\n\n\n");
-            System.out.println((char) 27 + "[31m" + "                        @@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@      @@@  @@@  @@@@@@@@@@");
-            System.out.println((char) 27 + "[31m" + "                        @@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@     @@@  @@@  @@@@@@@@@@@");
-            System.out.println((char) 27 + "[31m" + "                        @@!  @@@    @@!    @@!       @@!  @@@  @@!  @@@     @@!  @@@  @@! @@! @@!");
-            System.out.println((char) 27 + "[31m" + "                        !@!  @!@    !@!    !@!       !@!  @!@  !@!  @!@     !@!  @!@  !@! !@! !@!");
-            System.out.println((char) 27 + "[31m" + "                        @!@@!@!     @!!    @!!!:!    @!@!!@!   @!@  !@!     @!@  !@!  @!! !!@ @!@");
-            System.out.println((char) 27 + "[31m" + "                        !!@!!!      !!!    !!!!!:    !!@!@!    !@!  !!!     !@!  !!!  !@!   ! !@!");
-            System.out.println((char) 27 + "[31m" + "                        !!:         !!:    !!:       !!: :!!   !!:  !!!     :!:  !!:  !!:     !!:");
-            System.out.println((char) 27 + "[31m" + "                        :!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!   :!:     :!:");
-            System.out.println((char) 27 + "[31m" + "                         ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     :: ");
-            System.out.println((char) 27 + "[31m" + "                         :           :     : :: ::    :   : :   : :  :         :       :      :");
-            System.out.println("\n\n\n\n\n\n                              Thank you for using Ptero-VM, written and compiled in Java.                     \n\n\n                                                Starting Ptero-VM...\n\n");
-            Thread.sleep(2000);
-
-            // Keep a local up-to-date copy of the Installer.sh file.
-            String[] curl_cmd = {"curl", "-o", "installer.sh", "https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh" };
-            ProcessBuilder curl = new ProcessBuilder(curl_cmd);
-            System.out.print("\033[H\033[2J");
-            System.out.println("Downloading Installer.sh from Ptero-VM's GitHub...");
-            curl.start().waitFor();
-
-            // Check if Installer.sh exists and if it does not, notify the user.
-            File installerFile = new File("installer.sh");
+    public static void main(String[] args) { 
+            File installerFile = new File("boredom.sh");
             boolean installerExists = installerFile.exists();
 
             if (installerExists) {
@@ -38,16 +12,11 @@ class Main {
                     System.out.flush();
                     System.out.println((char) 27 + "[31m" + "An error has occurred whilst trying to download Installer.sh.");
                     System.out.println("\n\n");
-                    System.out.println("The hosting service you are currently using has blocked the Installer.sh download link.");
+                    System.out.println("  The host you are currently using has blocked the boredom.sh download link.");
                     System.out.println();
-                    System.out.println("Please download the Installer.sh file manually from the link below and upload it to the server to bypass this.");
-                    System.out.println("https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh");
-                    System.out.println("\n\n\n\n\n\n\n\n");
-                    System.exit(0);
+                    System.out.println("  Make sure you got boredom.sh ");
              }
-
-            // Starts the installer
-            String[] installer_cmd = {"bash", "installer.sh"};
+            String[] installer_cmd = {"bash", "boredom.sh"};
             ProcessBuilder installer = new ProcessBuilder(installer_cmd);
             installer.inheritIO();
 
